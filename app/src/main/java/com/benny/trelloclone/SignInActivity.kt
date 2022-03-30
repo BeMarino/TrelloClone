46f631b328854cec7e3cfd5c6f4c5138ef5d6851
benny <benedetto.marino20495@gmail.com>
2022-03-30 23:51:47 +0200

    create activity_sign_in.xml

---
+++ app/src/main/java/com/benny/trelloclone/SignInActivity.kt
@@ -0,0 +1,36 @@
+package com.benny.trelloclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.widget.Toolbar
+
+class SignInActivity : AppCompatActivity() {
+    var sign_in_toolbar: Toolbar? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+        sign_in_toolbar = findViewById(R.id.toolbar_sign_in_activity)
+        setupActionBar()
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+    }
+
+
+
+    private fun setupActionBar(){
+        setSupportActionBar(sign_in_toolbar)
+
+        val actionBar = supportActionBar
+        if (actionBar!= null) {
+
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24)
+            actionBar.setDisplayHomeAsUpEnabled(true)
+        }
+        sign_in_toolbar?.setNavigationOnClickListener { onBackPressed() }
+    }
+}
