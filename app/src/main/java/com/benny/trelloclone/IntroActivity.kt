a59160aa50272c79687c710676cf01ec78512edc
benny <benedetto.marino20495@gmail.com>
2022-03-30 23:50:22 +0200

    add onClickListener on signIn button

--- app/src/main/java/com/benny/trelloclone/IntroActivity.kt
+++ app/src/main/java/com/benny/trelloclone/IntroActivity.kt
@@ -9,11 +9,13 @@ import android.widget.Button
 class IntroActivity : AppCompatActivity() {
 
     var btn_sign_up:Button? = null
+    var btn_sign_in:Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
         btn_sign_up = findViewById(R.id.btn_sign_up_intro)
+        btn_sign_in = findViewById(R.id.btn_sign_in_intro)
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
@@ -21,6 +23,9 @@ class IntroActivity : AppCompatActivity() {
 
         btn_sign_up?.setOnClickListener {
             startActivity(Intent(this,SignUpActivity::class.java))
+        }
+        btn_sign_in?.setOnClickListener {
+            startActivity(Intent(this,SignInActivity::class.java))
         }
 
     }
