e72ebb5714e4e57ea4355984a38c0c17c98c105c
benny <benedetto.marino20495@gmail.com>
2022-04-09 14:16:59 +0200

    make SignInActivity inherit from BaseActivity

--- app/src/main/java/com/benny/trelloclone/activities/SignInActivity.kt
+++ app/src/main/java/com/benny/trelloclone/activities/SignInActivity.kt
@@ -1,13 +1,26 @@
 package com.benny.trelloclone.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.view.WindowManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import com.benny.trelloclone.R
+import com.benny.trelloclone.firebase.FirestoreClass
+import com.benny.trelloclone.models.User
+import com.google.firebase.auth.FirebaseAuth
 
-class SignInActivity : AppCompatActivity() {
-    var sign_in_toolbar: Toolbar? = null
+class SignInActivity : BaseActivity() {
+    private var sign_in_toolbar: Toolbar? = null
+    private lateinit var et_email: EditText
+    private lateinit var et_password: EditText
+    private lateinit var auth: FirebaseAuth
+    private lateinit var btn_sign_in: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,15 +32,29 @@ class SignInActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+
+        et_email = findViewById(R.id.sign_in_et_email)
+        et_password = findViewById(R.id.sign_in_et_password)
+
+        btn_sign_in = findViewById(R.id.btn_sign_in)
+        btn_sign_in.setOnClickListener { signInRegisteredUser() }
     }
 
 
+                    }
+                }
 
-    private fun setupActionBar(){
+        }
+    }
+
+    private fun setupActionBar() {
         setSupportActionBar(sign_in_toolbar)
 
         val actionBar = supportActionBar
-        if (actionBar!= null) {
+        if (actionBar != null) {
 
             actionBar.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24)
             actionBar.setDisplayHomeAsUpEnabled(true)
